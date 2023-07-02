69e203e750dc88fe9dbd03195bbbffccb79e9702
fidojahfalprayoga <fidojahfalprayoga@gmail.com>
2023-07-02 15:43:00 +0700

    tidying up some style file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./style.css";
-import "./style.css.map";
-import "./font-awesome.min.css";
+import "./style/css/style.css";
+import "./style/css/style.css.map";
+import "./style/css/font-awesome.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
